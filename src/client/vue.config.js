5913b038b5ee3de1294620f802df011a091a9a81
Hamza <hi@hamzarizvi.com>
2026-02-06 09:23:28 +0500

    Fix JS error: Alias @ohrm/oxd to ES module to fix mitt dependency resolution

--- src/client/vue.config.js
+++ src/client/vue.config.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const DumpBuildTimestampPlugin = require('./scripts/plugins/DumpBuildTimestampPlugin');
 
 module.exports = {
@@ -15,6 +17,7 @@ module.exports = {
       alias: {
         '@ohrm/core': '@/core',
         '@ohrm/components': '@/core/components',
+        '@ohrm/oxd$': path.resolve(__dirname, 'node_modules/@ohrm/oxd/index.es.js'),
       },
     },
     plugins: [new DumpBuildTimestampPlugin()],
