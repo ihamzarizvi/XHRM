28ceffa8bdc5c4b25a1382e6b9179672fa4c7403
Hamza <hi@hamzarizvi.com>
2026-02-08 22:04:17 +0500

    Force emergency error logging to root folder

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/emergency_error.log');
+
 include_once('./src/config/log_settings.php');
 
 use XHRM\Config\Config;
