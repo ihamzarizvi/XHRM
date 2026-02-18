8c1b2e98564ce930b0ce0f4ba03019c757458649
Hamza <hi@hamzarizvi.com>
2026-02-18 13:44:30 +0500

    Fix: Remove VaultPermissionTrait from VaultAdminAPI - was breaking entire plugin DI container causing 403 on user-keys

--- src/plugins/XHRMPasswordManagerPlugin/Api/VaultAdminAPI.php
+++ src/plugins/XHRMPasswordManagerPlugin/Api/VaultAdminAPI.php
@@ -6,29 +6,16 @@ use XHRM\Core\Api\V2\Endpoint;
 use XHRM\Core\Api\V2\EndpointResourceResult;
 use XHRM\Core\Api\V2\Model\ArrayModel;
 use XHRM\Core\Traits\UserRoleManagerTrait;
-use XHRM\PasswordManager\Traits\Api\VaultPermissionTrait;
 use XHRM\PasswordManager\Traits\Service\PasswordManagerServiceTrait;
 
 class VaultAdminAPI extends Endpoint
 {
     use PasswordManagerServiceTrait;
     use UserRoleManagerTrait;
-    use VaultPermissionTrait;
-
-    protected function init()
-    {
-        $this->checkAdminAccess();
-    }
 
     public function getAll(): EndpointResourceResult
     {
         $stats = $this->getPasswordManagerService()->getAdminStats();
-        // Return as Resource, assuming singleton resource logic or misuse of REST
-        // Actually, returning ResourceResult from getAll might break Controller expectation.
-        // Let's try wrapping in CollectionResult if needed, or just ResourceResult.
-        // But GenericRestController likely expects CollectionResult for List operation.
-
-        // Let's return stats as properties of a single "Stats" object.
         return new EndpointResourceResult(ArrayModel::class, $stats);
     }
 }
