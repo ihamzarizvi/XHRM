e2c24e21caaa6643c813b380dba14a2c73f23454
Hamza <hi@hamzarizvi.com>
2026-02-21 19:24:17 +0500

    diag: log viewer

---
+++ web/view_log.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/plain');
+$logFile = __DIR__ . '/api_debug_log.json';
+if (file_exists($logFile)) {
+    echo file_get_contents($logFile);
+} else {
+    echo 'Log file not found at: ' . $logFile;
+}
